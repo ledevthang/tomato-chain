1e55c2a4afb1dd2a120a1781e483292076499cc5
VietNV <vietnv@twendeesoft.com>
2022-05-17 20:47:32 +0700

    add test function

--- test/TomatoChain.js
+++ test/TomatoChain.js
@@ -1,13 +1,13 @@
 const TomatoChain = artifacts.require("TomatoChain");
+const companyNames = ["Twendee Company", "Tomato Company"];
 
 contract("TomatoChain",(accounts)=>{
     let [alice, bob] = accounts;
     it("should be able to create a new new company", async () => {
         const contractInstance = await TomatoChain.new();
-        const companyNameBytes32=web3.utils.asciiToHex("Tweendee Company");
+        const companyNameBytes32=web3.utils.asciiToHex(companyNames[0]);
         const result = await contractInstance.setCompany(alice,companyNameBytes32
             , {from: alice,value:web3.utils.toWei("0.01", "ether")});
-        console.log(web3.utils.asciiToHex("Tweendee Company"))
         assert.equal(result.receipt.status, true);
         assert.equal(result.logs[1].args.company[1],companyNameBytes32);
     })
