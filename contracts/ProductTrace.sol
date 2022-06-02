c61454699706bac33bd621dd82ae051f28eb183d
VietNV <vietnv@twendeesoft.com>
2022-06-03 01:55:04 +0700

    add Product trace contract

--- contracts/ProductTrace.sol
+++ contracts/ProductTrace.sol
@@ -8,30 +8,39 @@ contract ProductTrace is TomatoCheck {
     bytes32 public constant RETAILER_ROLE = keccak256("RETAILER_ROLE");
 
     struct CompanyProduct {
+        address from;
+        address retailer;
         uint productId;
         bytes32 ownerRole;
         bool isApproved;
     }
 
-    mapping(address => uint[]) public companyProductIdMapping;
     mapping(address => mapping(uint => CompanyProduct)) public companyProductMapping;
 
     event transferProductEvent(address indexed _from, address indexed _to, uint indexed _productId);
+    event confirmProductEvent(address indexed _from, address indexed _to, uint indexed _productId);
 
     constructor() TomatoCheck() {}
 
-    modifier checkHasProduct(uint productID){
-        require(products[productID].Provider == msg.sender || companyProductMapping[msg.sender][productID].isApproved
+    modifier checkHasProduct(uint _productId){
+        require(products[_productId].Provider == msg.sender || companyProductMapping[msg.sender][_productId].isApproved
         , "Company don't exists this product!");
         _;
     }
 
     function transferProduct(address _from, address _to, uint _productId) external payable
     onlyRole(COMPANY_ROLE) checkHasProduct(_productId) {
-        require(!companyProductMapping[_to][_productId].isApproved, "The receiving company already exists the product!");
-        CompanyProduct memory companyProduct = CompanyProduct(_productId, RETAILER_ROLE, false);
-        companyProductIdMapping[_to].push(_productId);
+        require(companyProductMapping[_to][_productId].retailer!=address(0), "The receiving company already exists the product!");
+        CompanyProduct memory companyProduct = CompanyProduct(_from,_to,_productId, RETAILER_ROLE, false);
         companyProductMapping[_to][_productId] = companyProduct;
         emit transferProductEvent(_from, _to, _productId);
+    }
+    
+    function confirmProduct(uint _productId) external payable onlyRole(COMPANY_ROLE){
+        require(companyProductMapping[msg.sender][_productId].retailer!=address(0) , "The company has not received this product yet!");
+        require(!companyProductMapping[msg.sender][_productId].isApproved, "The company already confirm the product!");
+        CompanyProduct storage cp=companyProductMapping[msg.sender][_productId];
+        cp.isApproved=true;
+        emit confirmProductEvent(cp.from,cp.retailer,cp.productId);
     }
 }
